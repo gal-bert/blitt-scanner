53b878eb0d31e52d6d7d87feb91881b1cd442304
Gregorius Albert <galbert.djaja@gmail.com>
2022-12-22 13:57:17 +0700

    Add commit

--- Test Blitt/ViewController.swift
+++ Test Blitt/ViewController.swift
@@ -6,14 +6,84 @@
 //
 
 import UIKit
+import Vision
+import VisionKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, VNDocumentCameraViewControllerDelegate {
 
+    @IBOutlet weak var resultTextView: UITextView!
+    
+    var ocrRequest = VNRecognizeTextRequest()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultTextView.text = ""
+        configureOCR()
     }
 
+    @IBAction func scan(_ sender: Any) {
+        let scanVC = VNDocumentCameraViewController()
+        scanVC.delegate = self
+        present(scanVC, animated: true)
+    }
+    
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        guard scan.pageCount >= 1 else {
+            controller.dismiss(animated: true)
+            return
+        }
+        
+//        imageView.image = scan.imageOfPage(at: 0)
+        processImage(scan.imageOfPage(at: 0))
+        controller.dismiss(animated: true)
+    }
+    
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+        controller.dismiss(animated: true)
+    }
+    
+    func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+        controller.dismiss(animated: true)
+    }
+    
+    func processImage(_ image: UIImage) {
+        guard let cgImage = image.cgImage else { return }
 
+        resultTextView.text = ""
+        
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        do {
+            try requestHandler.perform([self.ocrRequest])
+        } catch {
+            print(error)
+        }
+    }
+    
+    
+    func configureOCR() -> Void {
+        ocrRequest = VNRecognizeTextRequest { (request, error) in
+            
+            guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+            
+            var ocrText = ""
+            for observation in observations {
+                guard let topCandidate = observation.topCandidates(1).first else { return }
+                ocrText += topCandidate.string + "\n"
+            }
+            
+            
+            DispatchQueue.main.async {
+                self.resultTextView.text = ocrText
+            }
+            
+        }
+        
+        ocrRequest.recognitionLevel = .accurate
+        ocrRequest.recognitionLanguages = ["en-US"]
+        ocrRequest.usesLanguageCorrection = true
+        
+    }
+    
 }
 
